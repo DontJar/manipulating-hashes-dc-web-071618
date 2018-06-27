58fbc3725e5fc944f7af9f13bf1bb45a5f6f0b68
Jon Schulman <schulman99@gmail.com>
2018-06-27 18:51:40 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,8 +1,9 @@
+
 def first_challenge
   contacts = {
     "Jon Snow" => {
       name: "Jon",
-      email: "jon_snow@thewall.we", 
+      email: "jon_snow@thewall.we",
       favorite_icecream_flavors: ["chocolate", "vanilla", "mint chip"],
       knows: nil
     },
@@ -13,10 +14,14 @@ def first_challenge
     }
   }
 
-  #your code here
+contacts.each do |a,b|
+  b.each do |c,d|
+    binding.pry
+    delete_if d == "strawberry"
+  end
+end
 
 
   #remember to return your newly altered contacts hash!
   contacts
 end
-
